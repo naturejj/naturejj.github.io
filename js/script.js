07e26ec4db9cdb6e111e22915b79c49e540dbcbf
naturejj <38741891+naturejj@users.noreply.github.com>
2018-06-27 19:48:08 +0900

    헤더위치 바꿈. 처음에 nav 보이게.

---
+++ js/script.js
@@ -0,0 +1,31 @@
+jQuery(function($){
+  $(window).on('resize',function(){
+    vw=$('.video-wrap');
+    vw.width($(this).width());
+    vw.height($(this).height()); 
+    T = $('#header').offset().top;
+    vt = $(this).height();
+  }).resize();
+  
+  $(window).on('scroll',function(){
+    
+    var H = $(this).scrollTop();
+    
+//    if(H>100) $('video').get(0).pause();
+//    else $('video').get(0).play();
+   
+    if(H>T){
+      $('#header').addClass('scroll');
+      if(H>vt){
+        $('#header').addClass('scroll-bg');
+      }else{
+        $('#header').removeClass('scroll-bg');
+      }
+    }else{
+      $('#header').removeClass('scroll');
+    }
+    
+  });
+  
+  
+});
